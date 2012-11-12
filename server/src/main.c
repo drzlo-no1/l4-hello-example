30e01a9a33ed7bfc37d339b06068312ee75540b2
drzlo <drzlo@cherry.(none)>
2012-11-12 11:23:35 +0400

    strict length check

--- server/src/main.c
+++ server/src/main.c
@@ -14,12 +14,16 @@ static int manual_syscall_print(const char *str) {
 	l4_msg_regs_t *mr;
 	l4_msgtag_t tag, ret;
 	size_t length;
+	unsigned long word_length;
 
 
 	if(str == NULL)
-		return -1;
+		return -L4_EINVAL;
 
 	length = strlen(str) + 1;
+	word_length = length/sizeof(l4_umword_t) + (length%sizeof(l4_umword_t) ? 1 : 0);
+	if(word_length > (L4_UTCB_GENERIC_DATA_SIZE-2)*sizeof(l4_umword_t))
+		return -L4_EBADPROTO;
 	
 	env = l4re_env();
 	mr = l4_utcb_mr();
@@ -28,12 +32,12 @@ static int manual_syscall_print(const char *str) {
 	mr->mr[1] = length;
 	memcpy(&(mr->mr[2]), str, length);
 
-	tag = l4_msgtag(L4_PROTO_LOG, (length/sizeof(mr->mr[2]) + (length%sizeof(mr->mr[2]) ? 1 : 0)) + 2, 0, 0);
+	tag = l4_msgtag(L4_PROTO_LOG, word_length+2, 0, 0);
 
 	// syscall
 	ret = l4_ipc_send(env->log, l4_utcb(), tag, L4_IPC_NEVER);
 
-	return l4_msgtag_has_error(ret) ? -1 : 0;
+	return -l4_error(ret);
 }
 
 int main(void) {
